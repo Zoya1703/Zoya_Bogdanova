f021d943a29b8247b18d84ba948f678fb34a8513
Zoya1703 <zbohdanova@yukon.org.ua>
2024-02-07 16:02:05 +0200

    commit2

---
+++ Js/main.js
@@ -0,0 +1,29 @@
+
+/*let year = prompt('Введіть вік'); 
+if (year > 0) {
+    if (year % 10 === 1 ) {
+        console.log(`${year} рік!`);
+    } else if (year >= 2 && year <= 4) {
+        console.log(`${year} роки!`);
+    } else if (year % 10 === 1) {
+        console.log(`${year} рік!`);
+    } else if (year % 10 >= 2 && year % 10 <= 4) {
+        console.log(`${year} роки!`);
+    } else {
+        console.log(`${year} років!`);
+    }
+}*/
+let year = prompt('Введіть вік'); 
+if (year > 0) {
+    if (year === "11" || year === "12" || year === "14") {
+        console.log(`${year} років!`);
+    } else if (year % 10 === 1 ) {
+        console.log(`${year} рік!`);
+    } else if (year >= 2 && year <= 4) {
+        console.log(`${year} роки!`);
+    }  else if (year % 10 >= 2 && year % 10 <= 4) {
+        console.log(`${year} роки!`);
+    }else {
+        console.log(`${year} років!`);
+    }
+}
