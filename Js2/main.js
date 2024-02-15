c11e9ec3d4729760141109576ec712cfe9b6f2e3
Zoya1703 <zbohdanova@yukon.org.ua>
2024-02-15 10:33:01 +0200

    commit servises

--- Js2/main.js
+++ Js2/main.js
@@ -1,44 +1,42 @@
+// Об'єкт зі списком послуг та їх вартістю
 var services = {
-    "стрижка": 60,
-    "гоління": 80,
+    "стрижка": 100,
+    "гоління": 30,
     "Миття голови": 100
 };
 
+// Додавання нової послуги
 services['Розбити скло'] = 200;
+console.log("Розбити скло");
 
-// Метод для обчислення загальної вартості послуг
-services.price = function() {
+services['Грязна шия'] = 200;
+console.log("Грязна шия");
+
+
+
+function totalPrice() {
     var total = 0;
-    for (var key in this) {
-        if (typeof this[key] === 'number') {
-            total += this[key];
+    for (var key in services) {
+        if (typeof services[key] === 'number') {
+            total += services[key];
         }
     }
     return total;
-};
+}
 
-// Метод для знаходження мінімальної вартості
-services.minPrice = function() {
-    var min = Infinity;
-    for (var key in this) {
-        if (typeof this[key] === 'number' && this[key] < min) {
-            min = this[key];
-        }
-    }
-    return min;
-};
 
-// Метод для знаходження максимальної вартості
-services.maxPrice = function() {
-    var max = -Infinity;
-    for (var key in this) {
-        if (typeof this[key] === 'number' && this[key] > max) {
-            max = this[key];
-        }
-    }
-    return max;
-};
+function minPrice() {
+    var prices = Object.values(services); 
+    return Math.round(...prices);
+}
 
-console.log("Загальна вартість послуг:", services.price() + " грн");
-console.log("Мінімальна вартість послуг:", services.minPrice() + " грн");
-console.log("Максимальна вартість послуг:", services.maxPrice() + " грн");
+
+function maxPrice() {
+    var prices = Object.values(services); 
+    return Math.round(...prices); 
+}
+
+
+console.log("Загальна вартість послуг:", totalPrice() + " грн");
+console.log("Мінімальна вартість послуг:", minPrice() + " грн");
+console.log("Максимальна вартість послуг:", maxPrice() + " грн");
